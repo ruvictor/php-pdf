d0061b19a0783d0b110ff26eaf06c66fda71f565
Victor <rusuvictor30@gmail.com>
2020-04-02 21:08:13 -0400

    Init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create PDF using PHP</title>
+</head>
+<body>
+    <form action='' method="POST">
+        <input type="text" name="name" /><br />
+        <input type="submit" value="ok" />
+    </form>
+</body>
+</html>
