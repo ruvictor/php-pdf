b88e4ad4ec40ddd3e54f71a28afbaeae93f0bb6e
Victor <rusuvictor30@gmail.com>
2020-04-02 21:23:35 -0400

    Example 1

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+if($_POST){
+    require('fpdf/fpdf.php');
+    $pdf = new FPDF();
+    $pdf -> AddPage();
+    // SetFont(font, style, size)
+    // Cell(width, height, content, nextline parametters, alignement[c - center])
+    $pdf->SetFont('Arial','B',16);
+    $pdf->Cell(40,10,'Hello World!');
+    $pdf->Output();
+    $pdf -> output();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
